3b3c8aa96fbe910aaf70cac0c2a59b41a7a20d59
Karel Asselman <asselman.karel@gmail.com>
2020-11-07 14:05:37 +0100

    cocktail index layout

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,19 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: %i[new edit show destroy]
+
   def index
+    @cocktails = params.include?(:query) ? Cocktail.where("lower(name) LIKE '%#{params[:query].downcase}%'") : Cocktail.all
   end
 
-  def show
+  def show; end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :description, :image)
   end
 
-  def new
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses, dependent: :destroy
   has_many :reviews
-  validates :name, uniqueness: true, presence: true, lenght: { minimum: 3 }
-  validates :description, presence: true, lenght: { minimum: 3 }
+  validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 3 }
   has_one_attached :image
 end
